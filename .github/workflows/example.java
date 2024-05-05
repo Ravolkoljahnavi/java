f557549c224e53e8b9d9ebbae4ee2a5b2748c18e
Ravolkoljahnavi <163414027+Ravolkoljahnavi@users.noreply.github.com>
2024-05-05 19:04:14 +0530

    Create example.java

---
+++ .github/workflows/example.java
@@ -0,0 +1,21 @@
+public class example {
+
+    public static void main(String args[])
+    {
+        char a = 'A';
+        byte b = 2;
+        short s = 22;
+        int i = 45;
+        float f = 5.123456f;
+        double d = 4.123456789d;
+        boolean g = true;
+
+        System.out.println("char:"+a);
+        System.out.println("byte:"+b);
+        System.out.println("short:"+s);
+        System.out.println("int:"+i);
+        System.out.println("float:"+f);
+        System.out.println("double:"+d);
+        System.out.println("boolean:"+g);
+    }
+}
