61a628486c5615fce1e89d32e6a4e4c927b465af
Ravolkoljahnavi <163414027+Ravolkoljahnavi@users.noreply.github.com>
2024-05-06 14:53:36 +0530

    Create comparisonoperator.java

---
+++ .github/workflows/comparisonoperator.java
@@ -0,0 +1,26 @@
+public class comparisonoperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        //we used if to print the statements only which are true
+        //if(a == b){
+            System.out.println( a == b);
+        //}
+        //if(a != b){
+            System.out.println(a != b);
+        //}
+        //if(a > b){
+            System.out.println(a > b);
+        //}
+        //if(a >= b){
+            System.out.println(a >= b);
+        //}
+        //if(a < b){
+            System.out.println(a < b);
+        //}
+        //if(a <= b){
+            System.out.println(a <= b);
+        //}
+    }
+
+}
