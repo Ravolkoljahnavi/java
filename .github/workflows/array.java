335313e44fc861927d4dbe0b066591d9963aa982
Ravolkoljahnavi <163414027+Ravolkoljahnavi@users.noreply.github.com>
2024-05-06 14:52:07 +0530

    array.java

---
+++ .github/workflows/array.java
@@ -0,0 +1,28 @@
+public class array {
+    public static void main(String[] args) {
+        char[] anArray;
+        anArray = new char[10];//initializing the array size
+        anArray[0] = 'A';
+        anArray[1] = 'b';
+        anArray[2] = 'c';
+        anArray[3] = 'd';
+        anArray[4] = 'e';
+        anArray[5] = 'f';
+        anArray[6] = 'g';
+        anArray[7] = 'h';
+        anArray[8] = 'i';
+        anArray[9] = 'j';
+        System.out.println(anArray[0]);
+        System.out.println(anArray[1]);
+        System.out.println(anArray[2]);
+        System.out.println(anArray[3]);
+        System.out.println(anArray[4]);
+        System.out.println(anArray[5]);
+        System.out.println(anArray[6]);
+        System.out.println(anArray[7]);
+        System.out.println(anArray[8]);
+        System.out.println(anArray[9]);
+
+    }
+
+}
