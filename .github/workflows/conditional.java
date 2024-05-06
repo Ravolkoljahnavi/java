7a22d70503b48d9b641a91faa1c713d7bc312baa
Ravolkoljahnavi <163414027+Ravolkoljahnavi@users.noreply.github.com>
2024-05-06 14:54:22 +0530

    Create conditional.java

---
+++ .github/workflows/conditional.java
@@ -0,0 +1,14 @@
+public class conditional {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        if ((a == 3) & (b == 2)){
+            System.out.println("true");                    
+        }
+        if ((a == 5) || (b == 2)){
+            System.out.println("true");                    
+        }
+    }
+
+}
