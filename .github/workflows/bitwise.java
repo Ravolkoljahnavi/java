3f8ea94197aee3e5ef47e4881beaf6aef9a7da0e
Ravolkoljahnavi <163414027+Ravolkoljahnavi@users.noreply.github.com>
2024-05-06 14:52:53 +0530

    bitwise.java

---
+++ .github/workflows/bitwise.java
@@ -0,0 +1,11 @@
+public class bitwise {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        //5 is converted to 0101 and 3 is converted to 0011 and 'AND' operation 
+        System.out.println("a & b = " + (a & b));
+        //5 is converted to 0101 and 3 is converted to 0011 and 'OR' operation
+        System.out.println("a | b = " + (a | b));
+    }
+
+}
